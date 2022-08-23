aff2515ae0b91eeed44dc742d2baf3d012c6fa8f
Elbeth <tonyfav24@gmail.com>
2022-08-23 16:40:10 +0100

    added maxNum files

---
+++ maxNum/maxNum.js
@@ -0,0 +1,11 @@
+const myArray = [-5,-8,1,7,2,-9,5,4];
+let temp = 0;
+
+myArray.forEach((element) => {
+  if (temp < element) {
+    temp = element;
+  }
+});
+
+
+console.log(`The largest number in the array: ${temp}`);
